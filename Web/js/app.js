1b733a9f60016edc42670205ba8fba241737b107
SamEBae <baesammie@gmail.com>
2016-01-16 14:35:29 -0500

    set up

--- Web/js/app.js
+++ Web/js/app.js
@@ -1,7 +1,34 @@
-var app = angular.module('app', ['angularUtils.directives.dirPagination']);
-app.run(['$http', '$q', function ($http, $q) {
-}])
-.controller('defaultController', function($scope, $http) {
-	console.log("started!");
+var app = angular.module('app', ['angularUtils.directives.dirPagination','ngRoute']);
 
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/session', {
+        templateUrl: 'main.html',
+        controller: 'defaultController'
+      }).
+      when('/dashboard', {
+        templateUrl: 'dashboard.html',
+        controller: 'defaultController'
+      }).
+      otherwise({
+        redirectTo: '/session'
+      });
+  }]);
+
+app.controller('defaultController', function($scope, $http) {
+	$scope.sessionCreated = false;
+	$scope.sessionId = 12;
+	console.log($scope.sessionCreated);
+	$scope.createSession = function(){
+	 	$http.get('http://httpbin.org/get', {cache: null})
+	 	.success(function(response){
+        	$scope.sessionCreated = true;
+        	$scope.sessionId = 99;
+    	});
+	}
+});
+
+app.controller('dashboardController', function($scope, $http){
+	
 });
